18d8ca8fbe3b1f34036ce0a422cbc7080827ca14
Raymond <akdfhr2@gmail.com>
2024-06-14 11:02:10 +0900

    [MEDIUM] 00110 : medium-capitalize

---
+++ questions/00110-medium-capitalize/raymond.ts
@@ -0,0 +1,44 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<MyCapitalize<'foobar'>, 'Foobar'>>,
+  Expect<Equal<MyCapitalize<'FOOBAR'>, 'FOOBAR'>>,
+  Expect<Equal<MyCapitalize<'foo bar'>, 'Foo bar'>>,
+  Expect<Equal<MyCapitalize<''>, ''>>,
+  Expect<Equal<MyCapitalize<'a'>, 'A'>>,
+  Expect<Equal<MyCapitalize<'b'>, 'B'>>,
+  Expect<Equal<MyCapitalize<'c'>, 'C'>>,
+  Expect<Equal<MyCapitalize<'d'>, 'D'>>,
+  Expect<Equal<MyCapitalize<'e'>, 'E'>>,
+  Expect<Equal<MyCapitalize<'f'>, 'F'>>,
+  Expect<Equal<MyCapitalize<'g'>, 'G'>>,
+  Expect<Equal<MyCapitalize<'h'>, 'H'>>,
+  Expect<Equal<MyCapitalize<'i'>, 'I'>>,
+  Expect<Equal<MyCapitalize<'j'>, 'J'>>,
+  Expect<Equal<MyCapitalize<'k'>, 'K'>>,
+  Expect<Equal<MyCapitalize<'l'>, 'L'>>,
+  Expect<Equal<MyCapitalize<'m'>, 'M'>>,
+  Expect<Equal<MyCapitalize<'n'>, 'N'>>,
+  Expect<Equal<MyCapitalize<'o'>, 'O'>>,
+  Expect<Equal<MyCapitalize<'p'>, 'P'>>,
+  Expect<Equal<MyCapitalize<'q'>, 'Q'>>,
+  Expect<Equal<MyCapitalize<'r'>, 'R'>>,
+  Expect<Equal<MyCapitalize<'s'>, 'S'>>,
+  Expect<Equal<MyCapitalize<'t'>, 'T'>>,
+  Expect<Equal<MyCapitalize<'u'>, 'U'>>,
+  Expect<Equal<MyCapitalize<'v'>, 'V'>>,
+  Expect<Equal<MyCapitalize<'w'>, 'W'>>,
+  Expect<Equal<MyCapitalize<'x'>, 'X'>>,
+  Expect<Equal<MyCapitalize<'y'>, 'Y'>>,
+  Expect<Equal<MyCapitalize<'z'>, 'Z'>>,
+]
+
+// ============= Your Code Here =============
+/**
+ * intrinsic string manipulation types
+ *
+ * 문자열 조작을 돕기 위한 타입으로, typescript 컴파일러에 포함되어 있다.  `.d.ts`파일엔 유형 세트가 존재하지 않지만,
+ * 컴파일러에서 타입 추론 가능
+ *
+ */
+type MyCapitalize< S extends string> = S extends `${infer F}${infer Rest}` ? `${Uppercase<F>}${Rest}` : S
