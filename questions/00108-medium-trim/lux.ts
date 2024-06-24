98b40980c4710b443f441d230eb5af43e07deb5d
KumJungMin <37934668+KumJungMin@users.noreply.github.com>
2024-06-24 15:48:32 +0900

    [MEDIUM] 00108 : medium-trim

---
+++ questions/00108-medium-trim/lux.ts
@@ -0,0 +1,21 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Trim<'str'>, 'str'>>,
+  Expect<Equal<Trim<' str'>, 'str'>>,
+  Expect<Equal<Trim<'     str'>, 'str'>>,
+  Expect<Equal<Trim<'str   '>, 'str'>>,
+  Expect<Equal<Trim<'     str     '>, 'str'>>,
+  Expect<Equal<Trim<'   \n\t foo bar \t'>, 'foo bar'>>,
+  Expect<Equal<Trim<''>, ''>>,
+  Expect<Equal<Trim<' \n\t '>, ''>>,
+]
+
+
+// ============= Your Code Here =============
+type Space = '\n' | '\t' | ' ';
+type TrimLeft<S extends string> = S extends `${Space}${infer R}` ? TrimLeft<R> : S;
+type TrimRight<S extends string> = S extends `${infer R}${Space}` ? TrimRight<R> : S;
+
+type Trim<S extends string> = TrimRight<TrimLeft<S>>;
