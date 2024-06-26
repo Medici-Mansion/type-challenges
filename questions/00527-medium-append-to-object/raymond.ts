b01619fc0169eefd8af34c5ad498c052a7022506
Raymond <akdfhr2@gmail.com>
2024-06-26 16:45:14 +0900

    [MEDIUM] 00527 : medium-append-to-objects

---
+++ questions/00527-medium-append-to-object/raymond.ts
@@ -0,0 +1,53 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type test1 = {
+  key: 'cat'
+  value: 'green'
+}
+
+type testExpect1 = {
+  key: 'cat'
+  value: 'green'
+  home: boolean
+}
+
+type test2 = {
+  key: 'dog' | undefined
+  value: 'white'
+  sun: true
+}
+
+type testExpect2 = {
+  key: 'dog' | undefined
+  value: 'white'
+  sun: true
+  home: 1
+}
+
+type test3 = {
+  key: 'cow'
+  value: 'yellow'
+  sun: false
+}
+
+type testExpect3 = {
+  key: 'cow'
+  value: 'yellow'
+  sun: false
+  moon: false | undefined
+}
+
+type cases = [
+  Expect<Equal<AppendToObject<test1, 'home', boolean>, testExpect1>>,
+  Expect<Equal<AppendToObject<test2, 'home', 1>, testExpect2>>,
+  Expect<Equal<AppendToObject<test3, 'moon', false | undefined>, testExpect3>>,
+]
+
+// ============= Your Code Here =============
+type AppendToObject<T, U extends string, V> = {
+  // 왜 & 는 안되는걸까?
+  [k in keyof T | U]: k extends keyof T ? T[k] : V
+}
+
+type s = AppendToObject<test3, 'moon', false | undefined>
